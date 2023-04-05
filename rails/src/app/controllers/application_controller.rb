daaf7bc3aa094e2f6f3efafd8cd740aba42361c5
y-ogata mac <ogaty@users.noreply.github.com>
2023-04-05 11:41:00 +0900

    rails

--- rails/src/app/controllers/application_controller.rb
+++ rails/src/app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   def index
-    p current_user
 
+    @news_list = [
+      {
+        :id => 1,
+        :title => 'news1'
+      },
+      {
+        :id => 2,
+        :title => 'news2'
+      },
+      {
+        :id => 3,
+        :title => 'news3'
+      },
+    ]
   end
 end
